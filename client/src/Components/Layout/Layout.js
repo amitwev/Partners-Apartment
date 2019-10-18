827cef6918458d486045b250ac9d7c557237299e
amitwev@gmail.com <amit.salim@global-e.com>
2019-10-18 07:11:53 +0300

    add layout

---
+++ client/src/Components/Layout/Layout.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react'; 
+
+class Layout extends Component{
+    constructor(){
+        super();
+        this.state = { 
+            email:'',
+            firstName:'', 
+            lastName:'', 
+            phone:'',
+            hasHouse:'', 
+            
+        }
+    }
+}
+export default Layout; 
