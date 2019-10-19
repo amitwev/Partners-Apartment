992a071ec01bdaa155abeb323bbea84c52e6965d
amitwev <amitwev@gmail.com>
2019-10-19 12:52:56 +0300

    fix routing productin

--- index.js
+++ index.js
@@ -25,15 +25,14 @@ app.get('/api', (req, res) => {
         api: 'Version 1'
     })
 })
-// if (process.env.NODE_ENV === 'production') {
-//     // Serve any static files
-//     app.use(express.static(path.join(__dirname, 'client/build')));
-  
-//     // Handle React routing, return all requests to React app
-//     app.get('*', function(req, res) {
-//       res.sendFile(path.join(__dirname, 'client/build', 'index.html'));
-//     });
-//   }
+if (process.env.NODE_ENV === 'production') {
+    // Serve any static files
+    app.use(express.static(path.join(__dirname, 'client/build')));
+      // Handle React routing, return all requests to React app
+    app.get('*', function(req, res) {
+      res.sendFile(path.join(__dirname, 'client/build', 'index.html'));
+    });
+  }
 app.listen(PORT, () => {
     console.log(`app is running on port =  ${PORT}`)
 })
